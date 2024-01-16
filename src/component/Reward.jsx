c957745255e175beceb514368717360a5b1c64ef
shobola faruq <shobolafaruq@gmail.com>
2024-01-17 00:51:33 +0100

    Update reward section layout

--- src/component/Reward.jsx
+++ src/component/Reward.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Prize = ({ icon, title, aim }) => {
+export const Prize = ({ title, price, aim }) => {
   return (
-    <div className="group relative block h-40 sm:h-60">
+    <div className="group relative block h-40 sm:h-80">
       <span className="absolute inset-0 border-2 border-dashed border-black"></span>
 
       <div className="relative flex h-full transform items-end border border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
-        <div className="absolute p-4 transition-opacity group-hover:relative sm:p-6 lg:p-8">
+        <div className="absolute p-4 transition-opacity group-hover:relative sm:p-6 lg:p-8 lg:px-4">
           
           <h3 className="mt-4 text-xl capitalize font-medium sm:text-2xl">
             {title}
           </h3>
+          <p className="mt-1 text-gray-500 font-medium text-base">{price}</p>
 
           <p className="mt-4 text-sm sm:text-base">
             {aim}
@@ -23,6 +24,13 @@ export const Prize = ({ icon, title, aim }) => {
 };
 
 const Reward = () => {
+
+const [currency, setCurrency] = useState("USD");
+
+const handleCurrencyChange = (event) => {
+  setCurrency(event.target.value);
+};
+
   return (
     <div className="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-4">
@@ -32,42 +40,59 @@ const Reward = () => {
               Lorem, ipsum dolor sit amet consectetur adipisicing elit
             </h2>
 
-            <p className="hidden text-white/90 sm:mt-4 sm:block">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed.
-            </p>
-
-            <div className="mt-4 md:mt-8 w-full">
-              <a
-                href="/"
-                className="inline-block border border-white bg-white px-12 py-3 text-base font-medium text-black transition hover:bg-transparent hover:text-white"
-              >
-                Donate Now
-              </a>
+            <div className="mt-4 md:mt-8  w-full">
+              <form action="#" className="flex flex-wrap gap-4">
+                <div className="w-full flex border border-white bg-transparent">
+                  <select
+                    id="currency"
+                    name="currency"
+                    value={currency}
+                    onChange={handleCurrencyChange}
+                    className=" px-3 py-4 text-white text-lg lg:text-2xl font-semibold shadow-sm transition bg-transparent focus:outline-none"
+                  >
+                    <option value="USD" className="bg-black text-white">
+                      USD
+                    </option>
+                    <option value="EUR" className="bg-black text-white">
+                      EUR
+                    </option>
+                    <option value="GBP" className="bg-black text-white">
+                      GBP
+                    </option>
+                  </select>
+                  <input
+                    type="text"
+                    // placeholder="Email address"
+                    className="px-3 py-4 flex-1 flex-grow text-white text-base lg:text-2xl font-semibold text-right shadow-sm transition bg-transparent focus:outline-none"
+                  />
+                  <p className="px-3 py-4 text-white text-xl md:text-2xl lg:text-4xl text-semibold">
+                    .00
+                  </p>
+                </div>
+                <button className="inline-block border w-full border-white bg-white px-12 py-3 text-base font-medium text-black transition hover:bg-transparent hover:text-white">
+                  Donate Now
+                </button>
+              </form>
             </div>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap:10 p-5 md:p-8">
           <Prize
-            title="tier 1"
-            aim=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed."
+            title="bronze tier" price="($10 - $59)"
+            aim="Donors in the Bronze Tier will receive a personalized Thank You Email and will be recognized on the Fundraising Page."
           />
           <Prize
-            title="tier 2"
-            aim=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed."
+            title="silver tier" price="($60 - $199)"
+            aim=" Contributors to the Silver Tier will receive an exclusive Digital Badge or Certificate and a Social Media Shoutout."
           />
           <Prize
-            title="tier 3"
-            aim=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed."
+            title="gold tier" price="($200 - $999)"
+            aim=" Those in the Gold Tier will enjoy a customized Thank You Video from the Team and access to exclusive Behind-the-Scenes Content."
           />
           <Prize
-            title="tier 4"
-            aim=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et,
-              egestas tempus tellus etiam sed."
+            title="diamond tier" price="($1000 and above)"
+            aim=" Supporters in the Diamond Tier will receive special mention in Press Releases or Campaign Materials, along with VIP Access to an Event or Launch."
           />
         </div>
       </div>
