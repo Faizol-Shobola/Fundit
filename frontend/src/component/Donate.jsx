ddc7fb5f1f1f928bad318df4fd3abf3de1a895b4
Faizol Shobola <shobolafaizol@gmail.com>
2024-01-19 00:22:29 +0100

    Fix: notification

--- frontend/src/component/Donate.jsx
+++ frontend/src/component/Donate.jsx
@@ -74,19 +74,19 @@ const Donate = ({children}) => {
             <div className="flex items-center gap-4">
                 <span className="rounded-full bg-gray-100 p-2 text-gray-600">
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                >
-                    <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
-                    />
-                </svg>
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-user h-6 w-6"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+            </svg>
+
                 </span>
 
                 <div>
