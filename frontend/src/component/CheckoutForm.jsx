32c1b88d7f5a81747885e2655ea5afec2deda004
shobola faruq <shobolafaruq@gmail.com>
2024-01-20 09:36:32 +0100

    Update payment success message

--- frontend/src/component/CheckoutForm.jsx
+++ frontend/src/component/CheckoutForm.jsx
@@ -10,6 +10,7 @@ const CheckoutForm = ({amount}) => {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState("");
+  const [paymentSuccess, setPaymentSuccess] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,19 +31,22 @@ const CheckoutForm = ({amount}) => {
       setError(result.error.message);
     } else {
       // The PaymentElement handles the rest of the payment flow, including redirecting to your return URL.
+      setPaymentSuccess(true);
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <PaymentElement />
-      <button
-        disabled={!stripe}
-        className="btn mt-5 w-full"
-      >
+      <button disabled={!stripe} className="btn mt-5 w-full">
         Pay
       </button>
       {error && <div className="text-red-500">{error}</div>}
+      {paymentSuccess && (
+        <div className="text-xl text-green-500">
+          Payment Successful! Thank you for your payment.
+        </div>
+      )}
     </form>
   );
 };
